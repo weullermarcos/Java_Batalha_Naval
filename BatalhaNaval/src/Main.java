d24ed6c5d3fbfb6709f854cf78ba245f0446f551
Weuller Marcos da Silva Santos <weuller@redecfa.org.br>
2017-04-17 18:47:13 -0300

    lendo letra e numero

--- BatalhaNaval/src/Main.java
+++ BatalhaNaval/src/Main.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				
+		Scanner scanner = new Scanner(System.in);
 		Tabuleiro tabuleiro = new Tabuleiro();
+		Coordenada coordenada = new Coordenada();
 		
 		tabuleiro.DesenhaCabecalho();
 		tabuleiro.DesenhaTabuleiro();
-
+		
+		System.out.println("\nDigite uma letra: ");
+		coordenada.letra = scanner.nextLine().toUpperCase();
+		
+		System.out.println("\nDigite um número: ");
+		coordenada.numero = scanner.nextLine();
+		
+//		System.out.println(coordenada.letra);
+//		System.out.println(coordenada.numero);
+		
 	}
 
 }

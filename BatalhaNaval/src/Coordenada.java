3d1ddf40dd2d33869a91bc02e31d90521aa881a9
Weuller Marcos da Silva Santos <weuller@redecfa.org.br>
2017-04-20 09:16:42 -0300

    adicionando projeto feito em sala

--- BatalhaNaval/src/Coordenada.java
+++ BatalhaNaval/src/Coordenada.java
@@ -17,4 +17,37 @@ public class Coordenada {
 		this.numero = numero;
 	}
 	
+	public boolean letraPermitida(){
+			
+		String letra = this.letra.toUpperCase();
+		
+		if(letra.equals("A") || letra.equals("B") ||
+		   letra.equals("C") || letra.equals("D") ||
+		   letra.equals("E") || letra.equals("F") ||
+		   letra.equals("G") || letra.equals("H") ||
+		   letra.equals("I") || letra.equals("J")){
+			
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public boolean numeroValido(){
+		
+		String numero = this.numero.toUpperCase();
+		
+		if(numero.equals("1") || numero.equals("2") ||
+		   numero.equals("3") || numero.equals("4") ||
+		   numero.equals("5") || numero.equals("6") ||
+		   numero.equals("7") || numero.equals("8") ||
+		   numero.equals("9") || numero.equals("0")){
+			
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 }
--- BatalhaNaval/src/Main.java
+++ BatalhaNaval/src/Main.java
@@ -3,24 +3,43 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 				
 		Scanner scanner = new Scanner(System.in);
 		Tabuleiro tabuleiro = new Tabuleiro();
 		Coordenada coordenada = new Coordenada();
+		boolean letraValida = false;
+		boolean numeroValido = false;
 		
 		tabuleiro.DesenhaCabecalho();
 		tabuleiro.DesenhaTabuleiro();
 		
-		System.out.println("\nDigite uma letra: ");
-		coordenada.letra = scanner.nextLine().toUpperCase();
+		//enquanto nao for digitado uma letra valida
+		while(!letraValida){
+			
+			System.out.println("\nDigite uma letra: ");
+			coordenada.letra = scanner.nextLine();
+			
+			letraValida = coordenada.letraPermitida();
+			
+			if(!letraValida){
+				System.out.println("\nLetra invalida, favor digitar uma letra valida ");
+			}
+		}
 		
-		System.out.println("\nDigite um número: ");
-		coordenada.numero = scanner.nextLine();
+		//enquanto nao for digitado uma letra valida
+		while(!numeroValido){
+			
+			System.out.println("\nDigite um numero: ");
+			coordenada.numero = scanner.nextLine();
+			
+			numeroValido = coordenada.numeroValido();
+			
+			if(!numeroValido){
+				System.out.println("\nNumero invalido, favor digitar uma numero valido ");
+			}
+		}
 		
-//		System.out.println(coordenada.letra);
-//		System.out.println(coordenada.numero);
+		
 		
 	}
-
 }
